f8f9855a77c71e2a0d286badae1f2e64b7b2774e
Murilo Maciel <murilomaciel06oficial@gmail.com>
2025-07-18 14:09:53 -0300

    atualizando AppDbContext.cs

--- GerenciadorDeTarefas/Data/AppDbContext.cs
+++ GerenciadorDeTarefas/Data/AppDbContext.cs
@@ -1,8 +1,11 @@
-using Microsoft.EntityFrameworkCore;
-using GerenciadorDeTarefas.Models;
-
-public class AppDbContext : DbContext
+namespace GerenciadorDeTarefas.Data
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-    public DbSet<Tarefa> Tarefas { get; set; }
+    using Microsoft.EntityFrameworkCore;
+    using GerenciadorDeTarefas.Models;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
 }
